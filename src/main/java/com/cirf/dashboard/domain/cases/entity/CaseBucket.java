df9c50cb552777a305c79de924d28b58f7712ced
Yubin <qls773322@naver.com>
2025-11-22 16:06:19 +0900

    [FEAT/KAN-253] 버킷 정보를 담을 CaseBucket 엔티티 생성

---
+++ src/main/java/com/cirf/dashboard/domain/cases/entity/CaseBucket.java
@@ -0,0 +1,36 @@
+package com.cirf.dashboard.domain.cases.entity;
+
+import lombok.*;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CaseBucket {
+
+    private String pk;
+    private Long userId;
+    private Long caseId;
+    private String bucketName;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("PK")
+    public String getPk() {
+        return pk;
+    }
+
+    @DynamoDbAttribute("user_id")
+    public Long getUserId() { return userId; }
+
+    @DynamoDbAttribute("case_id")
+    public Long getCaseId() { return caseId; }
+
+    @DynamoDbAttribute("bucket_name")
+    public String getBucketName() { return bucketName; }
+
+}
