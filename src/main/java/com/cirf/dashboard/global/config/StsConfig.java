7a0685a9d871f246a0dc3d0885b82bd7ead0e3cb
kimcloyeon <rucy8103@naver.com>
2025-12-08 17:33:40 +0900

    DefaultCredentailsProvider로 수정

--- src/main/java/com/cirf/dashboard/global/config/StsConfig.java
+++ src/main/java/com/cirf/dashboard/global/config/StsConfig.java
@@ -3,19 +3,12 @@ package com.cirf.dashboard.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sts.StsClient;
 
 @Configuration
 public class StsConfig {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
@@ -24,9 +17,7 @@ public class StsConfig {
     public StsClient stsClient() {
         return StsClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
